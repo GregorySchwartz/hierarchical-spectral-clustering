baa187b959e6986b30399004cae5d5b88760106b
Gregory W. Schwartz <gsch@pennmedicine.upenn.edu>
2018-01-12 10:28:37 -0500

    Update tests to work with new criteria.

---
+++ src/Math/Clustering/Hierarchical/Spectral/Test.hs
@@ -0,0 +1,99 @@
+{- Math.Clustering.Hierarchical.Spectral.Test
+Gregory W. Schwartz
+
+Collects the functions pertaining to testing hierarchical spectral clustering.
+-}
+
+{-# LANGUAGE BangPatterns #-}
+
+module Math.Clustering.Hierarchical.Spectral.Test where
+
+-- Remote
+import Data.List (tails)
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import Math.Clustering.Spectral.Sparse (getB, B (..))
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Sparse.Common as S
+import qualified Data.Vector as V
+import qualified Numeric.LinearAlgebra as H
+
+-- Local
+import Math.Clustering.Hierarchical.Spectral.Types
+import Math.Clustering.Hierarchical.Spectral.Sparse
+import qualified Math.Clustering.Hierarchical.Spectral.Dense as Dense
+
+newtype QGram = QGram { unQGram :: String } deriving (Eq, Ord, Read, Show)
+newtype QGramMap = QGramMap
+    { unQGramMap :: Map.Map QGram Int
+    } deriving (Eq,Ord,Read,Show)
+
+exampleData :: [String]
+exampleData = [ "600 MOUNTAIN AVENUE"
+              , "700 MOUNTAIN AVE"
+              , "600-700 MOUNTAIN AVE"
+              , "100 DIAMOND HILL RD"
+              , "100 DIAMOND HILL ROAD"
+              , "123 SPRINGFIELD AVENUE"
+              , "123 SPRINFGIELD AVE"
+              ]
+
+exampleItems :: V.Vector String
+exampleItems = V.fromList exampleData
+
+-- | Add beginning and ending symbols.
+addBorders :: String -> String
+addBorders x = "##" <> x <> "$$"
+
+-- | Generate qgrams for a string.
+getQGrams :: Int -> String -> [QGram]
+getQGrams n =
+    fmap QGram . filter ((== n) . length) . fmap (take n) . tails . addBorders
+
+-- | Get mapping of qgrams to indices.
+getQGramMap :: [QGram] -> QGramMap
+getQGramMap = QGramMap . Map.fromList . flip zip [0,1..]
+
+-- | Convert a record to a vector.
+recordToRow :: Int -> QGramMap -> String -> S.SpVector Double
+recordToRow n (QGramMap qgramMap) = S.fromListSV (Map.size qgramMap)
+                                  . Map.toAscList
+                                  . Map.fromListWith (+)
+                                  . flip zip [1,1..]
+                                  . fromMaybe (error "Invalid qgram.")
+                                  . mapM (flip Map.lookup qgramMap)
+                                  . getQGrams n
+
+-- | Generate the matrix of qgrams from a list of records and qgram length.
+exampleMatrix :: Int -> [String] -> S.SpMatrix Double
+exampleMatrix n records =
+    S.transposeSM . S.fromColsL . fmap (recordToRow n qgramMap) $ records
+  where
+    qgramMap = getQGramMap
+             . Set.toList
+             . Set.fromList
+             . concatMap (getQGrams n)
+             $ records
+
+--clusterExample :: ClusteringTree String ShowB
+clusterExample = hierarchicalSpectralCluster
+                    Nothing
+                    exampleItems
+                    (Left $ exampleMatrix 3 exampleData)
+
+adjacencyExample :: S.SpMatrix Double
+adjacencyExample = (unB b) S.##^ (unB b)
+  where
+    b = getB $ exampleMatrix 3 exampleData
+
+denseAdjacencyExample :: H.Matrix Double
+denseAdjacencyExample = H.assoc (S.dimSM adjacencyExample) 0
+                      . fmap (\(!x, !y, !z) -> if x == y then ((x, y), 0) else ((x, y), z))
+                      . S.toListSM
+                      $ adjacencyExample
+
+denseClusterExample = Dense.hierarchicalSpectralCluster
+                        Nothing
+                        exampleItems
+                        denseAdjacencyExample
